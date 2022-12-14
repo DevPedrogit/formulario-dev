4b496c67c8af103109c59030fe66b13026dd05da
henrique.dev <pedro.dev.public2022@gmail.com>
2022-12-14 20:52:36 -0300

    Commit add function validationEmail

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
-function invalidated () {
+function invalidatedName () {
     let validateInputName = document.querySelector('#name');
     validateInputName.style.borderBottom = '2px solid red';
 
-    let spanValidate = document.querySelector('.spanValidate');
+    let spanValidate = document.querySelector('.spanValidateName');
     spanValidate.style.display = 'block';
-}
 
-function validate () {
+}
+function validateName () {
     let validateInputName = document.querySelector('#name');
     validateInputName.style.borderBottom = '2px solid black';
 
-    let spanValidate = document.querySelector('.spanValidate');
+    let spanValidate = document.querySelector('.spanValidateName');
     spanValidate.style.display = 'none';
 }
 
-
-function validation () {
-    let inputName = document.getElementById('name').value;
+function validationName () {
+    let inputName = document.querySelector('#name').value;
     
     if (inputName.length < 3) {
-        invalidated();
+        invalidatedName();
     } else {
-        validate();
+        validateName();
     }
 }
+
+// Validation email
+
+function invalidatedEmail () {
+    let validateInputEmail = document.querySelector('#email');
+        validateInputEmail.style.borderBottom = '2px solid red';
+
+    let spanValidateEmail = document.querySelector('.spanValidateEmail');
+    spanValidateEmail.style.display = 'block';
+}
+function validateEmail () {
+    let validateInputEmail = document.querySelector('#email');
+        validateInputEmail.style.borderBottom = '2px solid black';
+
+    let spanValidateEmail = document.querySelector('.spanValidateEmail');
+    spanValidateEmail.style.display = 'none';
+}
+
+function validationEmail () {
+    let inputEmail = document.querySelector('#email').value;
+    const emailRegex = /\S+@\S+\.\S+/;
+
+     if (!emailRegex.test(inputEmail)) {
+        invalidatedEmail();
+     } else {
+        validateEmail();
+     }
+  };
